13c44ae2b986e68eb2104b120713823636b2968b
Artur Ścibor <diaxmen90@gmail.com>
2024-07-05 14:36:57 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full flex justify-center">
+    <p class="text-2xl font-bold">Tamplate-vue3-pinia-tailwind-cypress</p>
+    <p>test github</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
